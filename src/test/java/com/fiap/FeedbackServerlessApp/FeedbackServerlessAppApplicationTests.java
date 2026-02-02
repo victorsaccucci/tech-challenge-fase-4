50391e97c57e350c6b53fbbb2f92c2d5b113f6be
victorsaccucci <victorsaccucciv@gmail.com>
2026-02-02 15:56:50 -0300

    first commit

---
+++ src/test/java/com/fiap/FeedbackServerlessApp/FeedbackServerlessAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.FeedbackServerlessApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackServerlessAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
